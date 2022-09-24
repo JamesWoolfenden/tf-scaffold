60403191ce43c0802568939e2d39b4921a2514d2
James Woolfenden <james.woolfenden@gmail.com>
2022-09-24 16:43:28 +0100

    update

--- example/examplea/terraform.tf
+++ example/examplea/terraform.tf
@@ -1,9 +1,9 @@
 terraform {
   required_providers {
     aws = {
-      version = "3.72.0"
+      version = "4.31.0"
       source  = "hashicorp/aws"
     }
   }
-  required_version = ">= 0.14.8"
+  required_version = ">= 1.3.0"
 }
